22e5d900ccaf0e275e424e6b185bc7602054b5dd
Sami Lappalainen <lappalainen.sami@outlook.com>
2024-09-11 20:00:29 +0200

    PrintSquare symbol colors

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -8,25 +8,47 @@ class Program
         PrintDisplay(InitializeDisplay(40, 12));
     }
 
-    private static void PrintSquare(char symbol, string color)
+    private static void PrintSquare(char symbol)
     {
-        if (color == "red")
+        switch (symbol)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.White;
+            case 'o':
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Red;
+                break;
+            case 'F':
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                break;
+            case '1':
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                break;
+            case '2':
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                break;
+            case '3':
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                break;
+            case '4':
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                break;
+            case '5':
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            case '6':
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
         }
 
         Console.Write(symbol);
+        Console.ResetColor();
     }
 
     private static void PrintLine(string line)
     {
         for (int j = 0; j < line.Length; j++)
         {
-            PrintSquare(line[j], "red");
+            PrintSquare(line[j]);
             if (j < line.Length - 1)
             {
                 Console.Write(" ");
