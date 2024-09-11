453a220be7006994256c138208c8f646b083255c
Sami Lappalainen <lappalainen.sami@outlook.com>
2024-09-11 18:52:45 +0200

    added PrintSquare()

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -2,8 +2,41 @@
 
 class Program
 {
+    // 40 by 12 field
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string line = "########################################";
+
+        for (int i = 0; i < 12; i++)
+        {
+            foreach (char c in line)
+            {
+                Console.Write(c + " ");
+            }
+            Console.Write("\n");
+        }
+    }
+
+    static void PrintSquare(string backgroundColor, string textColor, char symbol)
+    {
+        if (backgroundColor == "gray")
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        
+        if (textColor == "red")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        Console.Write(symbol);
     }
 }
